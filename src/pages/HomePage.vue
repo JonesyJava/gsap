048dc91422f711455f75c401aa7be0a47bd9eb22
Michael Jones <jonesyjava@gmail.com>
2021-08-19 15:21:32 -0600

    GSAP effects Tutorial

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo">
-    <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
-      <span class="mx-2 text-white">Vue 3 Starter</span>
-    </h1>
+    <transition appear @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+      <h1>Hello</h1>
+    </transition>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore rem sunt amet mollitia ratione, est esse. Totam nobis facere sed, aperiam consectetur illum ea ratione, suscipit alias ab cupiditate possimus.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis sunt quia illum assumenda laboriosam veritatis necessitatibus tenetur repellat maiores ipsa hic libero voluptates ad, id quam earum qui repellendus ratione!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores ab ipsa placeat ut itaque, debitis voluptatibus harum rem natus eum voluptatem, perferendis mollitia esse neque sed quibusdam? Nisi, consequuntur. Illo.</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import gsap from 'gsap'
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    const beforeEnter = (el) => {
+      console.log('before enter - set initial state')
+      el.style.transform = 'translateY(-60px)'
+      el.style.opacity = 0
+    }
+    const enter = (el, done) => {
+      console.log('starting to enter - make transition')
+      gsap.to(el, {
+        duration: 3,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+      })
+    }
+    const afterEnter = () => {
+      console.log('after enter')
+    }
+    return { beforeEnter, enter, afterEnter }
+  }
 }
 </script>
 
